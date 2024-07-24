da1cdc06f4a58a1aa51c0a863375ed1777c46f05
blake-shuttle <blake@shuttlelabs.xyz>
2024-07-24 08:09:18 -0400

    config: configured the project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-green-500 text-lg text-center">Hellow World</Text>
+    </View>
+  );
+};
+
+export default index;
